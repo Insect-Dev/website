764d640bdbc5c2df67f1ff87b0a1b8e552f15af0
mineTomek <102610423+mineTomek@users.noreply.github.com>
2024-12-11 21:30:29 +0100

    Add 3 placeholder sections

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,14 @@ export default function Home() {
     <div className="">
       <main className="h-dvh">
         <Hero />
+        {["about us", "projects", "social"].map((item) => (
+          <div
+            key={item}
+            className="flex h-dvh items-center justify-center border-t border-black p-5 text-4xl"
+          >
+            {item}
+          </div>
+        ))}
       </main>
       <footer className="flex flex-wrap items-center justify-center">
         Footer
