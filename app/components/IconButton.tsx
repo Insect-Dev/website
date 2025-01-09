d415a1c3188c94bf6e4d7743eab39ae04f458cdd
mineTomek <102610423+mineTomek@users.noreply.github.com>
2025-01-09 10:40:33 +0100

    Use zinc instead of gray in the buttons

--- app/components/IconButton.tsx
+++ app/components/IconButton.tsx
@@ -5,8 +5,8 @@ export default function IconButton(props: {
   icon: JSX.ElementType
 }) {
   return (
-    <Button className="group m-2 flex items-center overflow-hidden rounded-full bg-white px-1 py-1 text-sm font-medium text-gray-700 shadow-md transition-all hover:bg-gray-50 hover:text-gray-900 hover:shadow-lg active:bg-gray-100 active:shadow-inner">
-      <span className="flex h-8 w-8 items-center justify-center rounded-full bg-black text-white transition-all group-hover:bg-gray-900 group-active:bg-gray-800 group-active:shadow">
+    <Button className="group m-2 flex items-center overflow-hidden rounded-full bg-white px-1 py-1 text-sm font-medium text-zinc-700 shadow-md transition-all hover:bg-zinc-50 hover:text-zinc-900 hover:shadow-lg active:bg-zinc-100 active:shadow-inner">
+      <span className="flex h-8 w-8 items-center justify-center rounded-full bg-black text-white transition-all group-hover:bg-zinc-900 group-active:bg-zinc-800 group-active:shadow">
         <props.icon className="h-4 w-4" />
       </span>
       <span className="px-3">{props.text}</span>
